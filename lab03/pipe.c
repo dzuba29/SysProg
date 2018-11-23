cf9298b25880a296f9fd274695310f071f28415d
NickP <chepik29rus@gmail.com>
2018-11-23 20:09:43 +0300

    small fixes pipe.c , change sending from parent

--- lab03/pipe.c
+++ lab03/pipe.c
@@ -4,16 +4,25 @@
 
 int main(){
     int fd[2];
+    int fd1[2];
     size_t size;
     char string[] = "Hello, world!\n";
     pid_t forked_pid;
     char buffer[15];
+
+    //pipes
     if(pipe(fd) < 0)
     {
         printf("Can\'t create pipe\n");
         exit(-1);
     }
+    if(pipe(fd1) < 0)
+    {
+        printf("Can\'t create pipe\n");
+        exit(-1);
+    }
 
+    //fork
     forked_pid = fork();
     if (forked_pid == -1) 
     {   
@@ -23,16 +32,16 @@ int main(){
     else if (forked_pid == 0) 
     {   
         //Child proc
-        close(fd[0]);
-        write(fd[1],string,14);
-        exit(-1);
+        close(fd[1]);
+        size = read(fd[0],buffer,15);
+        fputs(buffer,stdout);
     } 
     else 
     {
         //Parent proc
-        close(fd[1]);
-        size = read(fd[0],buffer,15);
-        fputs(buffer,stdout);
+        close(fd[0]);
+        write(fd[1],string,14);
+        exit(-1);
     }
 
     return 0;
